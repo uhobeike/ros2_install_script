2a0a9311b9e22a5df016a774db60dc78ea877e02
Tatsuhiro Ikebe <beike315@icloud.com>
2024-05-12 12:47:40 +0900

    chore: Add humble

---
+++ humble/install_server.sh
@@ -0,0 +1,41 @@
+#/bin/bash -xve
+
+sudo apt update && sudo apt install curl
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install -y ros-humble-ros-base python3-colcon-common-extensions python3-rosdep2
+
+echo -e "\n## Execute ${BASH_SOURCE[0]} $(date +'%Y/%m/%d %H:%M:%S')"  >> ~/.bashrc
+
+grep "source /opt/ros/humble/setup.bash" ~/.bashrc
+if [ $? = 0 ]; then
+  :
+else
+  echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+fi
+
+grep "export ROS_DOMAIN_ID" ~/.bashrc
+if [ $? = 0 ]; then
+  :
+else
+  echo "export ROS_DOMAIN_ID=1" >> ~/.bashrc
+fi
+
+grep "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" ~/.bashrc
+if [ $? = 0 ]; then
+  :
+else
+  echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >> ~/.bashrc
+fi
+
+grep "export _colcon_cd_root=" ~/.bashrc
+if [ $? = 0 ]; then
+  :
+else
+  echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
+  echo "export _colcon_cd_root=~" >> ~/.bashrc
+fi
