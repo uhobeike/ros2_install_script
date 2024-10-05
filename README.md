8ee2d03581a101a256bebbd7f4778660cfc5e483
Tatsuhiro Ikebe <beike315@icloud.com>
2024-10-05 17:15:20 +0900

    chore: add apt install ros-dev-tools (#3)
    * chore: add apt-install ros-dev-tools
    
    * chore: add install how to
    
    * chore: fix readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ros2_install_script
+
+## Humble
+* install_desktop.sh
+```
+bash <(curl -s https://raw.githubusercontent.com/uhobeike/ros2_humble_install_script/main/install_desktop.sh)
+```
+
+* install_server.sh
+```
+bash <(curl -s https://raw.githubusercontent.com/uhobeike/ros2_humble_install_script/main/install_server.sh)
+```
--- humble/install_desktop.sh
+++ humble/install_desktop.sh
@@ -1,13 +1,12 @@
 #/bin/bash -xve
 
-sudo apt update && sudo apt install curl
+sudo apt update -y && sudo apt install -y curl
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt install -y ros-humble-desktop python3-colcon-common-extensions python3-rosdep2
+sudo apt install -y ros-humble-desktop ros-dev-tools
 sudo apt -y install gazebo
 sudo apt install -y ros-humble-gazebo-*
 
